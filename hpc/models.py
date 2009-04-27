ff17bd128d770b8997fe8281506bec14379f59e7
Zeke Harris <thalin@gmail.com>
2009-04-27 13:40:02 -0400

    Added Cluster & Queue, added fields to HPCJob.

--- hpc/models.py
+++ hpc/models.py
@@ -6,6 +6,28 @@ from django.utils.encoding import smart_str, smart_unicode
 from hpc.fields import UUIDField
 from hpc.exceptions import HPCError
 
+class Cluster(models.Model):
+    '''
+    This model represents a connectable cluster that has been configured to
+    allow access to a specific user via a specified public/private key pair.
+    '''
+    SCHEDULER_CHOICES = (
+        ('LSF', 'LSF'),
+    )
+    name = models.TextField(blank=False)
+    username = models.TextField(blank=False)
+    scheduler = models.TextField(blank=False, choices=SCHEDULER_CHOICES)
+
+class Queue(models.Model):
+    '''
+    This model represents a job queue on the Cluster.  Jobs should be
+    submitted to a specific Queue if possible.
+    '''
+    name = models.TextField(editable=False)
+    cluster = models.ForeignKey(Cluster)
+    available = models.BooleanField(help_text="Determines if this queue is available for job submission")
+    max_time = models.IntegerField("Maximum time limit", null=True)
+
 class HPCJob(models.Model):
     '''
     This model represents an HPC job.  To use this model you must inherit from
@@ -16,6 +38,10 @@ class HPCJob(models.Model):
     You may also override the setup and cleanup methods which are run before
     and after the run_job method, respectively.
     '''
+    PLATFORM_CHOICES = (
+        ('32-bit', 'x86'),
+        ('64-bit', 'x86-64'),
+    )
     uuid = UUIDField(auto=True)
     processing = models.BooleanField(editable=False)
     completed = models.BooleanField(editable=False)
@@ -24,6 +50,12 @@ class HPCJob(models.Model):
     debug_msg = models.TextField(editable=False, blank=True)
     created = models.DateTimeField(editable=False)
     modified = models.DateTimeField(editable=False)
+    jobid = models.IntegerField(editable=False)
+    nproc = models.IntegerField("Requested number of processors", default=1)
+    memory = models.IntegerField("Requested amount of memory (in MB)", null=True)
+    timelimit = models.IntegerField("Requested execution time (in minutes)", null=True)
+    jobname = models.TextField("Optional job name", blank=True)
+    platform = models.TextField(choices=PLATFORM_CHOICES)
 
     class Meta:
         abstract = True
